6a6ec3945641e6f62cc26b3c7b11f976e1e589e6
amansharma8944 <103628514+amansharma8944@users.noreply.github.com>
2023-01-01 12:10:43 +0530

    Add files via upload

---
+++ aman.js
@@ -0,0 +1,28 @@
+let toggle=document.getElementsByClassName("navbar");
+let h=false;
+
+
+
+function togglee(){
+let hh=document.querySelector("body");
+mainn.to
+hh.display.backgroundColor="red";
+
+
+}
+console.log(toggle);
+
+let btnnn=document.getElementsByClassName("btnn");
+btnnn.addEventListener("click",heyy);
+console.log(btnnn);
+
+function heyy(){
+
+
+let hhhh=document.getElementsByClassName("to-display");
+console.log(hhhh);
+hhhh.style.display="none";
+
+
+
+}
